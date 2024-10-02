62f9c8b5c540e13f572a174f5f7f70a9af430c56
dcode2100 <dikshantsolanki214@gmail.com>
2024-10-02 22:47:38 +0530

    git add

--- src/components/accountTable/index.js
+++ src/components/accountTable/index.js
@@ -1,15 +1,13 @@
-import { AccountTable } from "./AccountTable";
-import { FilterDrawer } from "./FilterDrawer";
-import { FilterSelect } from "./FilterSelect";
-import { FilterButton } from "./FilterButton";
-import { dateRangeToPill } from "./dateRangeToPill";
-import { FilterDateSelect } from "./FilterDateSelect";
+import { AccountTable } from "@/components/accountTable/AccountTable"
+import { FilterDrawer } from "@/components/accountTable/FilterDrawer"
+import { FilterSelect } from "@/components/accountTable/FilterSelect"
+import { FilterButton } from "@/components/accountTable/FilterButton"
+import { FilterDateSelect } from "@/components/accountTable/FilterDateSelect"
 
 export {
     AccountTable,
     FilterDrawer,
     FilterSelect,
     FilterButton,
-    dateRangeToPill,
     FilterDateSelect
 }
